46cbf2144686fe1c2419001271e91d166467b43b
Olamidun <kolapoolamidun@gmail.com>
2021-12-19 02:53:36 +0100

    tested the dojah api integration

--- sender/airtime_sender.py
+++ sender/airtime_sender.py
@@ -6,29 +6,55 @@ from .models import AirtimeUser
 
 
 dotenv.load_dotenv()
-def send_airtime(phone_number, amount, destination):
+def fetch_list_of_banks():
     app_id = os.getenv('appid')
-    secret_key = os.getenv('test_private_key')
-    headers  = {
-        'Accept': 'text/plain',
-        'Authorization': secret_key,
-        'AppId': app_id,
-        'Content-Type': 'application/json',
+    secret_key = os.getenv('production_secret_key')
+    print(secret_key)
+    headers = {
+        "Accept": "text/plain",
+        "AppId": "5fc144c4318b66003e7644c2",
+        "Authorization": "prod_sk_O5ZaI2rKmA1u4niXtJWUeJsoQ"
     }
-    url = 'https://sandbox.dojah.io/api/v1/purchase/airtime'
-    payload = {
-        "amount": amount,
-        "destination": destination
+    # url = f'https://sandbox.dojah.io/api/v1/kyc/phone_number/basic?phone_number={phone_number}'
+
+    url = f'https://api.dojah.io/api/v1/general/banks'
+    # payload = {
+    #     "amount": amount,
+    #     "destination": destination
+    # }
+    # try:
+    response = requests.get(url, headers=headers)
+    return {
+        'status': True,
+        'data': response.text
     }
-    try:
-        response = requests.post(url, json=payload, headers=headers)
-        return {
-            'status': True,
-            'data': response.json()
-        }
-    except:
-        return False
+    # except:
+    #     return False
 
 
         #     "Accept": "text/plain",
     # "Content-Type": "application/json"
+
+
+def send_airtime(amount, destination):
+    app_id = os.getenv('appid')
+    secret_key = os.getenv('production_secret_key')
+    print(secret_key)
+    headers = {
+        "Accept": "text/plain",
+        "AppId": app_id,
+        "Authorization": secret_key
+    }
+    # url = f'https://sandbox.dojah.io/api/v1/kyc/phone_number/basic?phone_number={phone_number}'
+
+    url = f'https://api.dojah.io/api/v1/purchase/airtime'
+    payload = {
+        "amount": amount,
+        "destination": destination
+    }
+    # try:
+    response = requests.post(url, headers=headers, json=payload)
+    return {
+        'status': True,
+        'data': response.text
+    }
--- sender/views.py
+++ sender/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import requests
 
 # Create your views here.
+
+def bank_and_airtime_callback(request):
+    if request.method == 'POST':
+        session_id = request.POST.get("sessionId", None)
+        service_code = request.POST.get("serviceCode", None)
+        phone_number = request.POST.get("phoneNumber", None)
+        text = request.POST.get("text", "default")
+
+        input = text.split('*')
