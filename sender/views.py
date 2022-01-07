49bcedb9553e15b2344169cbcb1dfdc5de5fff2c
Olamidun <kolapoolamidun@gmail.com>
2022-01-07 16:03:17 +0100

    debugging

--- sender/views.py
+++ sender/views.py
@@ -35,11 +35,12 @@ def crypto_ussd_callback(request):
                 response += "1. US Dollars USD\n"
                 return HttpResponse(response)
             else:
+                input = input.pop()
                 print(input)
                 print(len(input))
-                # response = "CON Invalid input. Must either be 1 or 2. \n"
-                response = "CON 1. To check price of cryptocurrency in your preferred currency\n"
-                response += "2. To check the exchange rate of your currency with other currencies\n"
+                response = "CON Invalid input. Try again\n"
+                response += "1. To check price of cryptocurrency in your preferred currency\n"
+                response += "2. To check exchange rate of your currency with other currencies"
                 
                 return HttpResponse(response)
         elif len(input) == 2:
