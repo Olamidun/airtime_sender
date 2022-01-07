c0de03e230f99e6f8b1a016727dbc310fc0040ca
Olamidun <kolapoolamidun@gmail.com>
2022-01-07 15:35:01 +0100

    testing new feature

--- sender/views.py
+++ sender/views.py
@@ -38,6 +38,7 @@ def crypto_ussd_callback(request):
                 response = "CON Invalid input. Must either be 1 or 2. \n"
                 response += "1. To check price of cryptocurrency in your preferred currency\n"
                 response += "2. To check the exchange rate of your currency with other currencies\n"
+                print(input)
                 return HttpResponse(response)
         elif len(input) == 2:
             if input[0] == "1":
