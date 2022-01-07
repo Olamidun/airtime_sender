5e30ffcf10678e2c198aa112a2a44be245ff1c1e
Olamidun <kolapoolamidun@gmail.com>
2022-01-07 15:44:10 +0100

    balksanksd

--- sender/views.py
+++ sender/views.py
@@ -34,7 +34,7 @@ def crypto_ussd_callback(request):
                 response += "1. US Dollars USD\n"
                 return HttpResponse(response)
             else:
-                response = "END Invalid input. Must either be 1 or 2. \n"
+                response = "CON Invalid input. Must either be 1 or 2. \n"
                 response += "1. To check price of cryptocurrency in your preferred currency\n"
                 response += "2. To check the exchange rate of your currency with other currencies\n"
                 print(input)
